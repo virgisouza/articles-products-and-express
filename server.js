a800b1ce60893c1ddb24edf693741767dc932c87
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-10-09 15:34:55 -1000

    set up completed for tests, routes

--- server.js
+++ server.js
@@ -9,9 +9,24 @@ app.set('view engine', '.hbs');
 const PORT = process.env.PORT || 3000;
 
 
+
 app.get('/', (req, res) => {
   res.send('Welcome to the jungle');
 });
+
+app.get('/articles/:id', (req,res) => {
+  console.log(req.params.id);
+});
+
+app.get('/prodicts/:id', (req,res) => {
+  console.log(req.params.id);
+});
+
+let articles = require('./routes/articles');
+app.use('/articles', articles);
+
+let products = require('./routes/products');
+app.use('/products', products);
 
 app.listen(3000, () => {
   console.log(`Server listening on port : ${PORT}`);
