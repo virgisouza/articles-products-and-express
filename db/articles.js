3ff79c19323fe2c3e7949850cc1ab383236ff866
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-10-11 15:39:30 -1000

    articles path: all functionality complete

--- db/articles.js
+++ db/articles.js
@@ -2,51 +2,55 @@ class Articles {
   constructor() {
     //{id: Number, name: String, price: Number, inventory: Number}
     this._articlesArr = [];
-    this._sku = 1;
+    this._urlTitle = 1;
   }
 
   getAllArticles() {
     return this._articlesArr;
   }
 
-  setId(product) {
-    product.price = product.price * 1;
-    product.inventory = parseInt(product.inventory);
-    product.id = this._sku++;
-    this._articlesArr.push(product);
+  setUrlTitle(article) {
+    //req = { title: String, body: String, author: String urlTitle: string}
+    let encoded = encodeURI(article.title);
+    article.urlTitle = encoded;
+    this._articlesArr.push(article);
     console.log(this._articlesArr);
   }
 
-  getArticleById(product) {
+  getArticleByTitle(article) {
     function search(nameKey, myArray){
       for (var i=0; i < myArray.length; i++) {
-          if (myArray[i].name === nameKey) {
+          if (myArray[i].title === nameKey) {
               return myArray[i];
             }
           }
         }
-    let result = search(product, this._articlesArr);
+        console.log(article);
+    let result = search(article, this._articlesArr);
     return result;
   }
 
-  editArticle(product, obj) {
+  editArticle(article, obj) {
     //create validation true / false
-    let foundObj = this.getArticleById(product);
+    let foundObj = this.getArticleByTitle(article);
     let grabObj = this._articlesArr.indexOf(foundObj);
-    this._productsArr.splice(grabObj, 1);
-    let matchId = foundObj.id;
+    this._articlesArr.splice(grabObj, 1);
+    let matchTitle = foundObj.title;
+    console.log(matchTitle);
     foundObj = obj;
-    obj.id = matchId;
-    obj.price = obj.price * 1;
-    obj.inventory = parseInt(obj.inventory);
+    obj.title = matchTitle;
+    obj.body = obj.body;
+    obj.author = obj.author;
+    let encoded = encodeURI(obj.title);
+    obj.urlTitle = encoded;
     this._articlesArr.push(obj);
     console.log(obj);
     console.log(this._articlesArr);
   }
 
-  deleteArticle(product) {
-    let foundObj = this.getArticleById(product);
-    let grabObj = this._productsArr.indexOf(foundObj);
+  deleteArticle(article) {
+    let foundObj = this.getArticleByTitle(article);
+    let grabObj = this._articlesArr.indexOf(foundObj);
     this._articlesArr.splice(grabObj, 1);
     console.log(this._articlesArr);
 
--- routes/articles.js
+++ routes/articles.js
@@ -12,25 +12,23 @@ router.get('/new', (req,res) => {
 });
 
 router.get('/:title', (req, res) => {
-  res.render('./articles/article', {oneArticle : Articles.getArticleById(req.params.title)});
+  console.log(req.params.title);
+  res.render('./articles/article', {oneArticle : Articles.getArticleByTitle(req.params.title)});
 });
 
 router.get('/:title/edit', (req, res) => {
-  res.render('./articles/edit', {editArticle : Articles.getArticleById(req.params.title)});
+  res.render('./articles/edit', {editArticle : Articles.getArticleByTitle(req.params.title)});
 });
 
 router.post('/new', (req,res) => {
-  //req = { name: String, price: String, inventory: String }
   //if (validation) {do this redirect}else{do this redirect}
-  Articles.setId(req.body);
+  Articles.setUrlTitle(req.body);
   res.render('./articles/new');
   console.log({'success' : true});
 });
 
 router.put('/:title/edit', (req, res) => {
-  //functionality works. need to push item back into array at same position so array stays organized in numeric order
   //if (validation) {do this redirect}else{do this redirect}
-
   Articles.editArticle(req.params.title, req.body);
   res.render('./articles/edit');
 });
